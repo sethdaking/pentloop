198dadfe97f8d82519cabdaeb314c598bd0e9327
sethdaking <lastemail768@gmail.com>
2024-07-27 21:24:42 +0200

    Heartbreak code

--- src/components/WordMark.tsx
+++ src/components/WordMark.tsx
@@ -2,7 +2,7 @@
 
 export default function WordMark() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width={300} height={75} viewBox="0 0 4160.000000 1181.000000" preserveAspectRatio="xMidYMid meet">
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width={200} height={50} viewBox="0 0 4160.000000 1181.000000" preserveAspectRatio="xMidYMid meet">
       
 
 
